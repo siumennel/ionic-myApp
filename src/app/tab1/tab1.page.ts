7e27fd8b250d83cab2d2a233ae945bc3fba52a73
楊建設 <yangjianshe@yangjianshedeMacBook-Pro.local>
2020-06-04 19:50:46 +0900

    push notification test

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,23 +1,70 @@
-import { Component, OnInit } from '@angular/core';
-import { MyPlugin  } from "my-plugin";
-import { Plugins } from '@capacitor/core';
+import { Component, OnInit } from "@angular/core";
+import { MyPlugin } from "my-plugin";
+import {
+  Plugins,
+  PushNotification,
+  PushNotificationToken,
+  PushNotificationActionPerformed,
+} from "@capacitor/core";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"],
 })
 export class Tab1Page implements OnInit {
-hello:string = "jian she1"
+  hello: string = "jian she1";
 
-  constructor() {
-  }
+  constructor() {}
 
   ngOnInit(): void {
-    const { MyPlugin } = Plugins;
-    MyPlugin.echo({ value: this.hello}).then((res: { value: string }) => {
+    const { MyPlugin, PushNotifications } = Plugins;
+    MyPlugin.echo({ value: this.hello }).then((res: { value: string }) => {
       this.hello = res.value;
-      console.log("res:" + this.hello)
-    })
+      console.log("res:" + this.hello);
+    });
+
+    console.log("Initializing HomePage");
+
+    // Request permission to use push notifications
+    // iOS will prompt user and return if they granted permission or not
+    // Android will just grant without prompting
+    PushNotifications.requestPermission().then((result) => {
+      if (result.granted) {
+        // Register with Apple / Google to receive push via APNS/FCM
+        PushNotifications.register();
+      } else {
+        // Show some error
+      }
+    });
+
+    // On success, we should be able to receive notifications
+    PushNotifications.addListener(
+      "registration",
+      (token: PushNotificationToken) => {
+        alert("Push registration success, token: " + token.value);
+      }
+    );
+
+    // Some issue with our setup and push will not work
+    PushNotifications.addListener("registrationError", (error: any) => {
+      alert("Error on registration: " + JSON.stringify(error));
+    });
+
+    // Show us the notification payload if the app is open on our device
+    PushNotifications.addListener(
+      "pushNotificationReceived",
+      (notification: PushNotification) => {
+        alert("Push received: " + JSON.stringify(notification));
+      }
+    );
+
+    // Method called when tapping on a notification
+    PushNotifications.addListener(
+      "pushNotificationActionPerformed",
+      (notification: PushNotificationActionPerformed) => {
+        alert("Push action performed: " + JSON.stringify(notification));
+      }
+    );
   }
 }
